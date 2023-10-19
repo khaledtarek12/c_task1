dae7fb829ebf85d6833854e1530ad02ff2117bf8
khaledtarek12 <134336536+khaledtarek12@users.noreply.github.com>
2023-10-19 20:28:03 +0300

    Add files via upload

--- khaledTask.c
+++ khaledTask.c
@@ -1,18 +1,30 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
 #define MAX_USERS 10
 #define MAX_USERNAME_LENGTH 20
 #define MAX_PASSWORD_LENGTH 20
 
+enum FlagType { INTEGER, BOOLEAN };
+
+struct Flag {
+    enum FlagType type;
+    union {
+        int intValue;
+        bool boolValue;
+    } value;
+};
+
 struct User {
     char username[MAX_USERNAME_LENGTH];
     char password[MAX_PASSWORD_LENGTH];
+    struct Flag flag;
 };
 
 struct User users[MAX_USERS];
 int numUsers = 0;
-
+char flagInput[10];
 void registerUser() {
     if (numUsers >= MAX_USERS) {
         printf("Maximum number of users reached. Cannot register new user.\n");
@@ -27,6 +39,27 @@ void registerUser() {
     printf("Enter password: ");
     scanf("%s", newUser.password);
 
+    printf("Enter flag (0 or 1 for integer, ( true , yes ) or  ( false , no ) for boolean): ");
+    scanf("%s", flagInput);
+
+    if (strcmp(flagInput, "0") == 0 || strcmp(flagInput, "1") == 0) {
+        newUser.flag.type = INTEGER;
+        newUser.flag.value.intValue = atoi(flagInput);
+    }
+    else if (strcmp(flagInput, "true") == 0 || strcmp(flagInput, "yes") == 0) {
+        newUser.flag.type = BOOLEAN;
+        newUser.flag.value.boolValue = true;
+    }
+    else if (strcmp(flagInput, "false") == 0 || strcmp(flagInput, "no") == 0) {
+        newUser.flag.type = BOOLEAN;
+        newUser.flag.value.boolValue = false;
+    }
+    else {
+        printf("Invalid flag input. Defaulting to 0 (integer).\n");
+        newUser.flag.type = INTEGER;
+        newUser.flag.value.intValue = 0;
+    }
+
     users[numUsers++] = newUser;
 
     printf("User registered successfully.\n");
@@ -36,21 +69,35 @@ void loginUser() {
     char username[MAX_USERNAME_LENGTH];
     char password[MAX_PASSWORD_LENGTH];
 
-    printf("Enter username: ");
-    scanf("%s", username);
+    if ((strcmp(flagInput, "true") == 0 || strcmp(flagInput, "yes") == 0 || strcmp(flagInput, "1") == 0)) {
+        printf("Enter username: ");
+        scanf("%s", username);
 
-    printf("Enter password: ");
-    scanf("%s", password);
+        printf("Enter password: ");
+        scanf("%s", password);
 
-    for (int i = 0; i < numUsers; i++) {
-        if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
-            printf("Login successful. Welcome, %s!\n", username);
-            return;
+        for (int i = 0; i < numUsers; i++) {
+            if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0) {
+                if (users[i].flag.type == INTEGER && users[i].flag.value.intValue == 0) {
+                    printf("You are not active. Login failed.\n");
+                }
+                else if (users[i].flag.type == BOOLEAN && users[i].flag.value.boolValue == false) {
+                    printf("You are not active. Login failed.\n");
+                }
+                else {
+                    printf("Login successful. Welcome, %s!\n", username);
+                }
+                return;
+            }
         }
     }
-
+    else {
+        printf("You are not active. Login failed.\n");
+    }
+    
     printf("Invalid username or password. Login failed.\n");
 }
+
 
 int main() {
     int choice;
@@ -81,3 +128,4 @@ int main() {
 
     return 0;
 }
+// if (strcmp(users[i].username, username) == 0 && strcmp(users[i].password, password) == 0)
